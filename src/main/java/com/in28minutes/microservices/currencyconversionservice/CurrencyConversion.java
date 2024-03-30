986a5f0be08c89bffcb1f80f23dcbe8220052024
DIR\mark.a.mckenna <mark.a.mckenna.contractor@bbva.com>
2024-03-30 16:49:15 +0100

    Section 8: Kubernetes with Microservices using Docker, Spring Boot and Spring Cloud

---
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,28 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CurrencyConversion {
+
+	private Long id;
+
+	private String from;
+
+	private String to;
+
+	private BigDecimal quantity;
+	
+	private BigDecimal conversionMultiple;
+
+	private BigDecimal totalCalculatedAmount;
+
+	private String envirnoment;
+
+}
